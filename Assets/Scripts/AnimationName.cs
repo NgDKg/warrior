ff04c92e28652034c6b34c65039d89e073d237da
NgDKg <ngduykhuong552002@gmail.com>
2024-11-04 19:30:40 +0700

    PlayerMoveAnimation

---
+++ Assets/Scripts/AnimationName.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+    internal class AnimationName
+    {
+        internal static string isMoving = "isMoving";
+        internal static string isRunning = "isRunning";
+    }
